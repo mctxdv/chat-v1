a79a51cbdf12b0421343cc5e878864afc6ab6fec
mctxdv <147929182+mctxdv@users.noreply.github.com>
2023-12-26 15:54:53 +0900

    Update loadscript.js

--- loadscript.js
+++ loadscript.js
@@ -1,43 +1,43 @@
-var now = new Date();
+document.addEventListener('DOMContentLoaded', function () {
+    const now = new Date();
+    const Year = now.getFullYear();
+    const Month = now.getMonth() + 1;
+    const day = now.getDate();
+    const Hour = now.getHours();
+    const Min = now.getMinutes();
+    const Sec = now.getSeconds();
 
-var Year = now.getFullYear();
-var Month = now.getMonth()+1;
-var Date = now.getDate();
-var Hour = now.getHours();
-var Min = now.getMinutes();
-var Sec = now.getSeconds();
+    console.log(document.location + ' が正常に読み込まれました。(' + Year + '年' + Month + '月' + day + '日' + Hour + ':' + Min + ':' + Sec + ')');
 
-window.onload = async function() {
-  console.log(document.location+'が正常に読み込まれました。(' + Year + "年" + Month + "月" + Date + "日" + Hour + ":" + Min + ":" + Sec+')');
-  
-  var openButton = document.getElementById('openButton');
-  if (openButton) {
-    var modal = document.getElementById('myModal');
-    openButton.onclick = function(e) {
-      e.preventDefault(); 
-      modal.style.display = 'block';
-    }
-    
-    function closeModal() {
-      modal.style.display = 'none';
-    }
-    
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = 'none';
-      }
-    }
-    
-    // navbarの高さを取得
-    var navbarHeight = document.getElementById('navbar').offsetHeight;
-    var navbarBreak = document.querySelector('.navbarbreak');
-    if (navbarBreak) {
-      navbarBreak.style.marginTop = navbarHeight + 'px';
-    }
-  }
+    const openButton = document.getElementById('openButton');
 
-  // DOMContentLoaded イベントで以下の処理を実行
-  document.addEventListener('DOMContentLoaded', function () {
+    if (openButton) {
+        const modal = document.getElementById('myModal');
+
+        openButton.onclick = function (e) {
+            e.preventDefault();
+            modal.style.display = 'block';
+        }
+
+        function closeModal() {
+            modal.style.display = 'none';
+        }
+
+        window.onclick = function (event) {
+            if (event.target == modal) {
+                modal.style.display = 'none';
+            }
+        }
+
+        // navbarの高さを取得
+        const navbarHeight = document.getElementById('navbar').offsetHeight;
+        const navbarBreak = document.querySelector('.navbarbreak');
+
+        if (navbarBreak) {
+            navbarBreak.style.marginTop = navbarHeight + 'px';
+        }
+    }
+
     const homeLink = document.getElementById('homeLink');
     const eventsLink = document.getElementById('eventsLink');
     const linksLink = document.getElementById('linksLink');
@@ -47,48 +47,49 @@ window.onload = async function() {
     const linksContent = document.getElementById('links');
 
     homeLink.addEventListener('click', function (e) {
-      e.preventDefault();
-      toggleContent(homeLink, homeContent);
+        e.preventDefault();
+        toggleContent(homeLink, homeContent);
     });
 
     eventsLink.addEventListener('click', function (e) {
-      e.preventDefault();
-      toggleContent(eventsLink, eventsContent);
+        e.preventDefault();
+        toggleContent(eventsLink, eventsContent);
     });
 
     linksLink.addEventListener('click', function (e) {
-      e.preventDefault();
-      toggleContent(linksLink, linksContent);
+        e.preventDefault();
+        toggleContent(linksLink, linksContent);
     });
 
     function toggleContent(link, content) {
-      if (!link.classList.contains('active')) {
-        // 選択されたリンクがアクティブでない場合
-        link.classList.add('active');
-        content.style.maxHeight = content.scrollHeight + 'px';
-      } else {
-        // 選択されたリンクが既にアクティブな場合
-        link.classList.remove('active');
-        content.style.maxHeight = null;
-      }
+        if (!link.classList.contains('active')) {
+            // 選択されたリンクがアクティブでない場合
+            link.classList.add('active');
+            content.style.maxHeight = content.scrollHeight + 'px';
+        } else {
+            // 選択されたリンクが既にアクティブな場合
+            link.classList.remove('active');
+            content.style.maxHeight = null;
+        }
     }
-  });
+
     const openModalBtn = document.getElementById("openModalBtn");
     const modal = document.getElementById("myModal");
     const closeBtn = document.querySelector(".close");
 
-    openModalBtn.addEventListener("click", function () {
-        modal.style.display = "block";
-    });
+    if (openModalBtn && modal && closeBtn) {
+        openModalBtn.addEventListener("click", function () {
+            modal.style.display = "block";
+        });
 
-    closeBtn.addEventListener("click", function () {
-        modal.style.display = "none";
-    });
-
-    window.addEventListener("click", function (event) {
-        if (event.target === modal) {
+        closeBtn.addEventListener("click", function () {
             modal.style.display = "none";
-        }
-    });
-}
+        });
 
+        window.addEventListener("click", function (event) {
+            if (event.target === modal) {
+                modal.style.display = "none";
+            }
+        });
+    }
+});
