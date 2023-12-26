381155a1ff7dc796dc892d7d9403a1e7b04ebc88
mctxdv <147929182+mctxdv@users.noreply.github.com>
2023-12-26 15:47:17 +0900

    Update loadscript.js

--- loadscript.js
+++ loadscript.js
@@ -74,3 +74,23 @@ window.onload = async function() {
     }
   });
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const openModalBtn = document.getElementById("openModalBtn");
+    const modal = document.getElementById("myModal");
+    const closeBtn = document.querySelector(".close");
+
+    openModalBtn.addEventListener("click", function () {
+        modal.style.display = "block";
+    });
+
+    closeBtn.addEventListener("click", function () {
+        modal.style.display = "none";
+    });
+
+    window.addEventListener("click", function (event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    });
+});
